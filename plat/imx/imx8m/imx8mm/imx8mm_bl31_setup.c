a9caa2cbe6000638ba9d109efe175f31c9334efc
Tim Harvey <tharvey@gateworks.com>
2021-10-18 10:40:29 -0700

    plat: imx8m{n,m}: remove UART4 from M4/M7 domain
    Gateworks Venice boards use UART4 thus they can not be allocated to the
    M4/M7 domain.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
+++ plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -58,7 +58,9 @@ static const struct imx_rdc_cfg rdc[] = {
 	RDC_MDAn(RDC_MDA_M4, DID1),
 
 	/* peripherals domain permission */
+#if 0
 	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+#endif
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
 
 	/* memory region */
